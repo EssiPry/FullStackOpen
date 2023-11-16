55c43e0c24123429533a1334a08e21dde70d5c9f
Essi Prykäri <essi.prykari@helsinki.fi>
2023-11-16 18:50:37 +0200

    adds exercise 1.6 unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,34 +1,47 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const Display = (props) => (
+  <div>{props.text} {props.number}
+  </div>
+)
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleClickGood = () => {
+    setGood(good +1)
+  }
+
+  const handleClickNeutral = () => {
+    setNeutral(neutral +1)
+  }
+  const handleClickBad = () => {
+    setBad(bad +1)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h2>give feedback</h2>
+
+      <Button handleClick={handleClickGood} text="good"/>
+      <Button handleClick={handleClickNeutral} text="neutral"/>
+      <Button handleClick={handleClickBad} text="bad"/>
+
+      <h2>statistics</h2>
+
+      <Display text="good" number={good}/>
+      <Display text="neutral" number={neutral}/>
+      <Display text="bad" number={bad}/>
+    </div>
   )
 }
 
