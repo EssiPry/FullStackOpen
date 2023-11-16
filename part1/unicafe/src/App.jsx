9f57e691efe4823ee71e829a191a1bef91006f19
Essi Prykäri <essi.prykari@helsinki.fi>
2023-11-16 21:49:24 +0200

    adds exercise 1.11 which changes statistic display to table

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -16,19 +16,26 @@ const Statistics = (props) => {
   }
   return (
     <div>
-      <StatisticLine text="good" value={props.good}/>
-      <StatisticLine text="neutral" value={props.neutral}/>
-      <StatisticLine text="bad" value={props.bad}/>
-      <StatisticLine text="all" value={props.total}/>
-      <StatisticLine text="average" value={props.average}/>
-      <StatisticLine text="positive" value={props.positive} percent="%"/>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={props.good}/>
+          <StatisticLine text="neutral" value={props.neutral}/>
+          <StatisticLine text="bad" value={props.bad}/>
+          <StatisticLine text="all" value={props.total}/>
+          <StatisticLine text="average" value={props.average}/>
+          <StatisticLine text="positive" value={props.positive} percent="%"/>
+        </tbody>
+      </table>
     </div>
   )
 }
 
 const StatisticLine = (props) => {
   return (
-    <p>{props.text} {props.value} {props.percent}</p>
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value} {props.percent}</td>
+    </tr>
   )
 }
 
