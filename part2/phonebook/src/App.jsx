3cc6b0c3d4294d63f093016f7c84ae296591fcb8
Essi Prykäri <essi.prykari@helsinki.fi>
2023-12-12 22:44:53 +0200

    adds exercise 2.13 which extracts backend communication into its own module

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,8 +3,10 @@ import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import personService from './services/persons'
 
 const App = () => {
+
   const [persons, setPersons] = useState([])
 
   const [newName, setNewName] = useState('')
@@ -12,14 +14,11 @@ const App = () => {
   const [filterUsed, setFilterUsed] = useState('')
 
   useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        console.log('promise fulfilled')
-        setPersons(response.data)
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
       })
-
   }, [])
 
   const addPerson = (event) => {
@@ -27,24 +26,22 @@ const App = () => {
     //console.log('add name', newName)
     //console.log('add number', newNumber)
     if (persons.some(person => person.name === newName)){
-      alert(`${newName} is already added to phonebook`)
+      alert(`${newName} is already added to the phonebook`)
     }
     else{
       const personObject =  {
           name: newName,
           number: newNumber
         }
-      axios
-        .post('http://localhost:3001/persons', personObject)
-        .then(response => {
-          console.log(response)
-        })
 
-    setPersons(persons.concat(personObject))
-    }
-    setNewName('')
-    setNewNumber('')
-  }
+        personService
+        .create(personObject)
+        .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson))
+          setNewName('')
+          setNewNumber('')
+        })
+  }}
 
   const personsToShow = filterUsed
     ? persons.filter(person => person.name.toLowerCase().includes(filterUsed.toLowerCase()))
