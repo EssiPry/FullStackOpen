ece485dd9a7b76a72ccaa1cce87a5794825e5433
Essi Prykäri <essi.prykari@helsinki.fi>
2024-03-08 23:18:18 +0200

    adds helper function to calculate total likes & unit tests. Exercise 4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce((total, blog) => {
+      return total += blog.likes
+    }, 0)
+}
+
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
