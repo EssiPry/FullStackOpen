f392da304fd5264ecae76d54f9a0cfe80fc4ff91
Essi Prykäri <essi.prykari@helsinki.fi>
2024-05-15 22:53:34 +0300

    adds user info to blogs & blog info to users. Exercise 4.17

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,8 +1,9 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (req, res) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', {username: 1, name: 1, id: 1 })
   res.json(blogs)
 })
 
@@ -21,13 +22,19 @@ blogsRouter.get('/:id', async (req, res) => {
 blogsRouter.post('/', async (req, res) => {
   const body = req.body
 
+  const user = await User.findById(body.userId)
+
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes || 0,
+    user: user._id
   })
+
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
 
   res.status(201).json(savedBlog)
 })
